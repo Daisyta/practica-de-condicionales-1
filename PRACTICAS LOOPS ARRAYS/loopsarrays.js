fb1a174f97fc493542425d372dfb971b96fefe30
Daisy Castillo <daisyta@MacBook-Air-de-Daisy.local>
2021-03-26 18:19:49 -0300

    tercer ejercicio loops arrays

--- PRACTICAS LOOPS ARRAYS/loopsarrays.js
+++ PRACTICAS LOOPS ARRAYS/loopsarrays.js
@@ -1,5 +1,5 @@
-var j = [2,4,6,8,10];
-for (var k = j.length - 1; k > 0; k--) {
-    console.log(k);
-    console.log(j[k]);
+var m = [2,4,6,8,10];
+for (var n = 0; n < m.length; n += 2) {
+    console.log(n);
+    console.log(m[n]);
 }
