ab32dbc7b70a77fee2eb79af063a27c50b721265
Daisy Castillo <daisyta@MacBook-Air-de-Daisy.local>
2021-03-26 18:24:12 -0300

    sexto ejercicio loops arrays

--- PRACTICAS LOOPS ARRAYS/loopsarrays.js
+++ PRACTICAS LOOPS ARRAYS/loopsarrays.js
@@ -1,7 +1,5 @@
-var r = [-1,0,5,-3,2];
-for (var s = 0; s < r.length; s++) {
-    if (r[s] > 0) {
-        r[s] = r[s] * r[s];
-    }
+var t = [];
+for (var u = 0; u < 4; u++) {
+    t.push(u);
 }
-console.log(r);
+console.log(t);
