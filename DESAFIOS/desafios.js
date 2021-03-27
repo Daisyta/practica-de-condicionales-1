8b4190207426b687401cc8161b3692320446ba71
Daisy Castillo <daisyta@MacBook-Air-de-Daisy.local>
2021-03-27 17:04:21 -0300

    debe imprimir todos los enteros desde el 1 hasta el 1000000

--- DESAFIOS/desafios.js
+++ DESAFIOS/desafios.js
@@ -5,3 +5,5 @@ function imprimeHasta(x) {
     } else {for (var x = 1 ; x >= 1; x++ ){
       console.log(x) ;
 }         }} imprimeHasta(1000000); // debe imprimir todos los enteros desde el 1 hasta el 1000000
+y = imprimeHasta(-10); 
+console.log(y); // debe imprimir 'false'
