21b2d89ade164cf36d9063e9a1868191e65075c5
Daisy Castillo <daisyta@MacBook-Air-de-Daisy.local>
2021-03-28 22:38:53 -0300

    introduccion a los fundamentos de la programacion

---
+++ INTRODUCCION A LOS FUNDAMENTOS DE LA PROGRAMACION/DESAFIOS/desafios.js
@@ -0,0 +1,9 @@
+function imprimeHasta(x) {
+
+    if (x < 1) {
+      return(false) ;  
+    } else {for (var x = 0 ; x >= 1; x++ ){
+      console.log(x) ;
+}         }} imprimeHasta(1000000); // debe imprimir todos los enteros desde el 1 hasta el 1000000
+y = imprimeHasta(-10); 
+console.log(y); // debe imprimir 'false'
